79f9d11214d7af27f5f67217e727c0af5c783444
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2021-02-17 13:36:13 +0100

    Another step to getting methods in component from app file

--- src/App.js
+++ src/App.js
@@ -4,24 +4,13 @@ import TodoList from './components/TodoList';
 import InputTodo from './components/InputTodo';
 import { v4 as uuidv4 } from 'uuid';
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
   const todoNameRef = useRef( );
   const [todos, setTodos] = useState([]);
-  
-    // getting stored todos from local storage
-    useEffect(() => {
-        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-        if (storedTodos){
-            setTodos(storedTodos);
-        }
-    }, []);
 
-    // storing todos in local storage
-    useEffect(() => {      
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-    }, [todos]);
+    // getting stored todos from local storage
+
   function handleAddTodo(e){
     const name = todoNameRef.current.value;
     if (name === '') return;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,20 @@
-import React  from 'react';
+import React , { useEffect } from 'react';
 import Todos from './Todos';
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 export default function TodoList({todos, setTodos}) {
 
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if (storedTodos){
+            setTodos(storedTodos);
+        }
+    }, []);
+
+    // storing todos in local storage
+    useEffect(() => {      
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    }, [todos]);
     function toggleTodo(id) {
         // copy of our todos
         const newTodos = [...{todos}];
